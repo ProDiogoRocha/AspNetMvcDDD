aa3896722c145a88685117228bd420f1127586d2
Diogo Mateus dos Santos Rocha <re035074@resource.com.br>
2018-03-22 16:41:52 -0300

    Primeiro Commit

---
+++ src/EP.CursoMvc.Infra.Data/EntityConfig/ClienteConfig.cs
@@ -0,0 +1,44 @@
+﻿using EP.CursoMvc.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.Infrastructure.Annotations;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EP.CursoMvc.Infra.Data.EntityConfig
+{
+    class ClienteConfig : EntityTypeConfiguration<Cliente>
+    {
+        public ClienteConfig()
+        {
+            HasKey(c => c.ClienteId);
+
+            Property(c => c.Nome)
+                .IsRequired()
+                .HasColumnOrder(1)
+                .HasMaxLength(150)
+                .HasColumnType("varchar")
+                .HasColumnName("str_Nome");
+
+            Property(c => c.CPF)
+                .IsRequired()
+                .HasMaxLength(11)
+                .IsFixedLength()
+                .HasColumnAnnotation("Index", new IndexAnnotation(
+                    new IndexAttribute() { IsUnique = true }));
+
+            Property(c => c.Email)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            Property(c => c.DataNascimento)
+                .IsRequired();
+
+            Property(c => c.Ativo)
+                .IsRequired(); 
+        }
+    }
+}
